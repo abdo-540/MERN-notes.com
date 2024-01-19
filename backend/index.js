1d82a3c1e53515afc458b1e6fcf7314f2e146da4
abdo-540 <abdelrahmanessam540@gmail.com>
2024-01-19 19:41:25 +0200

    minor adjustments

--- backend/index.js
+++ backend/index.js
@@ -7,3 +7,15 @@ import nodemailer from "nodemailer";
 import crypto from "crypto";
 
 dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json);
+app.use(cors());
+//app.use('/users', userRoute);
+
+app.get("/", (req, res) => {
+    console.log(req);
+    return res.status(234).send("welcome");
+});
