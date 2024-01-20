cf709f7fa317aeac7af36e7af1a606b4e758cab7
abdo-540 <abdelrahmanessam540@gmail.com>
2024-01-20 18:46:12 +0200

    did the notes backend

--- backend/routes/noteRoute.js
+++ backend/routes/noteRoute.js
@@ -3,12 +3,75 @@ import { Note } from "../models/note";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+    try {
+        const notes = await Note.find({});
+        return res.status(200).json(notes);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({message: err.message});
+    }
+});
 
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const note = await Note.findById(id);
+        return res.status(200).json(note);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({message: err.message});
+    }
+});
 
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+    try {
+        if(!req.body.title) {
+            return res.status(400).send({message: "Please enter the note's title"});
+        }
+        else{
+            const newNote = {
+                title: req.body.title,
+                body: req.body.body,
+                userID: req.body.id
+            };
+        const note = Note.create(newNote);
+        return res.status(201).json(note);
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({message: err.message});
+    }
+});
 
-router.put("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+    try {
+        if(!req.body.title){
+            return res.status(400).send({message: "Please enter the note's title"});
+        }
+        else{
+            const {id} = req.params;
+            const newNote = {
+                title: req.body.title,
+                body: req.body.body,
+                userID: req.body.id
+            };
+            const note = Note.findByIdAndUpdate(id, newNote);
+            return res.status(200).json(note);
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({message: err.message});
+    }
+});
 
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        await Note.findByIdAndDelete(id);
+        return res.status(200);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({message: err.message});
+    }
+});
